7af587bfab5ca0c7acc1a2176a5c674fa4aeeac2
Bianca Semprit <bsemprit@gmail.com>
2018-09-29 17:47:00 -0400

    Colors and selections fixed calendar month

--- src/app/features/calendar/Calendar.js
+++ src/app/features/calendar/Calendar.js
@@ -13,20 +13,16 @@ class Calendar extends Component {
 	
 	constructor(props) {
 		super(props);
+		let today = moment();
 		this.state = {
+			today,
 			selectedStartDate: null,
+			day: today.clone().startOf('month'),
+			customDatesStyles: [],
 		};
 		this.onDateChange = this.onDateChange.bind(this);
-		let today = moment();
-		let day = today.clone().startOf('month');
-		this.customDatesStyles = [];
-		while(day.add(1, 'day').isSame(today, 'month')) {
-			this.customDatesStyles.push({
-				date: day.clone(),
-				// Random colors
-				style: {backgroundColor: '#'+('#00000'+(Math.random()*(1<<24)|0).toString(16)).slice(-6)},
-			});
-		}
+		this.onMonthChange = this.onMonthChange.bind(this);
+		this.setCustomDateColors();
 	}
 	
 	static navigationOptions = {
@@ -34,27 +30,58 @@ class Calendar extends Component {
 	};
 	
 	onDateChange(date) {
+		console.log(date);
 		this.setState({
 			selectedStartDate: date,
 		});
 	}
 	
+	onMonthChange(date) {
+		console.log("month", date);
+		this.setState({
+			day: date.clone().startOf('month'),
+		});
+	}
+	
+	setCustomDateColors() {
+		const newState = this.state;
+		const day = moment(newState.day);
+		const lastDayOfMonth = moment(day).endOf('month').subtract(1, "days");
+		// console.log("date", this.state.day, day, Object.keys(lastDayOfMonth));
+		let startDay = day.clone();
+		for(let i = 0; i <= lastDayOfMonth.format('DD'); i++) {
+			newState.customDatesStyles.push({
+				date: startDay.clone(),
+				// Random colors
+				style: {backgroundColor: '#1e9cc3'},
+				textStyle: {color: 'white'}, // sets the font color
+				containerStyle: [],
+			});
+			startDay.add(1, 'day');
+		}
+	}
+	
 	render() {
-		const { selectedStartDate } = this.state;
-		const startDate = selectedStartDate ? selectedStartDate.toString() : '';
+		const { selectedStartDate, day } = this.state;
+		const startDate = selectedStartDate ? selectedStartDate.toString() : day.toString();
 		const today = new Date();
+		const selectedDay = moment(startDate).format("YYYY-MM-DD HH:mm:ss");
+		const selectedDayOfWeek = moment(startDate).format("dddd");
+		console.log("this.stae", this.state);
 		return (
 			<View style={styles.container}>
 				<CalendarPicker
 					todayTextStyle={{fontWeight: 'bold'}}
 					todayBackgroundColor={'transparent'}
-					customDatesStyles={this.customDatesStyles}
-					minDate={today}
+					customDatesStyles={this.state.customDatesStyles}
+					minDay={1}
 					onDateChange={this.onDateChange}
+					onMonthChange={this.onMonthChange}
 				/>
 				
 				<View>
-					<Text>SELECTED DATE:{ startDate }</Text>
+					<Text>{ startDate }</Text>
+					<Text>{ selectedDayOfWeek }</Text>
 				</View>
 			</View>
 		);
