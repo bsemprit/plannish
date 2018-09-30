a09661e535d29026ea4a3add7aaa376bf83d9322
Bianca Semprit <bsemprit@gmail.com>
2018-09-30 19:45:35 -0400

    Additional colors and fake events and start of day obj for list of events below calendar

--- src/app/features/calendar/Calendar.js
+++ src/app/features/calendar/Calendar.js
@@ -9,6 +9,73 @@ import moment from 'moment';
 
 import CalendarPicker from 'react-native-calendar-picker';
 
+function createFakeEvents() {
+	const startDay = moment().startOf('month');
+	const lastDayOfMonth = moment(startDay).endOf('month').subtract(1, "days");
+	const fakeEvents = new Map();
+	fakeEvents.set(startDay.format('L'), {
+			events: [{
+				color: "#e547e6",
+				title: "MLK celebration",
+				timeFrame: [startDay, startDay.endOf("day")],
+				reminders: [],
+				repeat: {
+					repeats: false,
+					times: [],
+				},
+				importance: ""
+			}]
+		});
+	for (i=0; i < lastDayOfMonth.format('DD'); i++) {
+		const day = startDay.add(1, 'day');
+		const nextDay = {
+			events: [{
+				color: "#e547e6",
+				title: "Reading",
+				timeFrame: [day, day.endOf("day")],
+				reminders: [],
+				repeat: {
+					repeats: false,
+					times: [],
+				},
+				importance: ""
+			},
+				{
+					color: "#7b46e6",
+					title: "Coding",
+					timeFrame: [day, day.endOf("day")],
+					reminders: [],
+					repeat: {
+						repeats: true,
+						times: [{
+							years: 1,
+						}],
+					},
+					importance: "!!"
+				}]
+		};
+		
+		if( i % 2 === 0) {
+			nextDay.events.push({
+				color: "#56e6e4",
+				title: "Do a Sun Salutation",
+				timeFrame: [day, day.set({h: 6, m: 30})],
+				reminders: [],
+				repeat: {
+					repeats: true,
+					times: [{
+						days: 2,
+					}],
+				},
+				importance: "!!"
+			})
+		}
+		fakeEvents.set(day.format('L'), nextDay);
+	}
+	return fakeEvents;
+};
+
+
 class Calendar extends Component {
 	
 	constructor(props) {
@@ -19,6 +86,7 @@ class Calendar extends Component {
 			selectedStartDate: null,
 			day: today.clone().startOf('month'),
 			customDatesStyles: [],
+			days: this.props.days || createFakeEvents(),
 		};
 		this.onDateChange = this.onDateChange.bind(this);
 		this.onMonthChange = this.onMonthChange.bind(this);
@@ -37,7 +105,7 @@ class Calendar extends Component {
 	}
 	
 	onMonthChange(date) {
-		console.log("month", date);
+		// console.log("month", monthdate);
 		this.setState({
 			day: date.clone().startOf('month'),
 		});
@@ -49,11 +117,30 @@ class Calendar extends Component {
 		const lastDayOfMonth = moment(day).endOf('month').subtract(1, "days");
 		// console.log("date", this.state.day, day, Object.keys(lastDayOfMonth));
 		let startDay = day.clone();
+		// console.log("here", this.state.days);
 		for(let i = 0; i <= lastDayOfMonth.format('DD'); i++) {
+			const dayEvent = this.state.days.get(startDay.format('L'));
+			const eventColors = dayEvent.events.map((event) => {return event.color});
+			console.log("eventColors", eventColors);
+			let style = {};
+			switch (eventColors.length) {
+				case 1:
+					style.backgroundColor = eventColors[0];
+					break;
+				case 2:
+					style.backgroundColor = eventColors[1];
+					break;
+					// style.background = `linear-gradient( 90deg, ${eventColors[0]}, ${eventColors[1]} )`; TODO: Find calendar that can support multiple colors or create own
+				case 3:
+					style.backgroundColor = eventColors[2];
+					break;
+					// style.background = `linear-gradient( -45deg,${eventColors[0]},${eventColors[0]} 33%,${eventColors[1]} 34%,${eventColors[1]} 66%,${eventColors[2]} 67%)`;
+				default:
+			}
 			newState.customDatesStyles.push({
 				date: startDay.clone(),
 				// Random colors
-				style: {backgroundColor: '#1e9cc3'},
+				style,
 				textStyle: {color: 'white'}, // sets the font color
 				containerStyle: [],
 			});
@@ -67,7 +154,7 @@ class Calendar extends Component {
 		const today = new Date();
 		const selectedDay = moment(startDate).format("YYYY-MM-DD HH:mm:ss");
 		const selectedDayOfWeek = moment(startDate).format("dddd");
-		console.log("this.stae", this.state);
+		// console.log("this.stae", createFakeEvents());
 		return (
 			<View style={styles.container}>
 				<CalendarPicker
